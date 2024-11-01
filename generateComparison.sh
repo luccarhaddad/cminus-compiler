7c443148039ed801b4ea35fcea5d018661d95784
luccarhaddad <luccarhaddad@gmail.com>
2024-10-31 22:50:02 -0300

    correct comparison script

--- generateComparison.sh
+++ generateComparison.sh
@@ -2,9 +2,6 @@
 cd build
 ../compLabAluno.bash
 cd ..
-mkdir "check" && cd check
-mkdir aluno && mkdir gabarito
-mv alunodetail/*_syn.txt check/aluno/
-mv detail/*_syn.txt check/gabarito/
-kdiff3 check/aluno/ check/gabarito
-rm -rf check/
+cp alunodetail/*_syn.txt check/aluno/
+cp detail/*_syn.txt check/gabarito/
+kdiff3 check/aluno/ check/gabarito/
