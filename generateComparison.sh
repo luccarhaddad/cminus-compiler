277623f2b8b77e69248e38e69e39f582405726b0
luccarhaddad <luccarhaddad@gmail.com>
2024-12-11 23:33:32 -0300

    setup for code generation

--- generateComparison.sh
+++ generateComparison.sh
@@ -6,13 +6,3 @@ make
 make ddiff
 cd ..
 python3 compare_diffs.py
-# shellcheck disable=SC2103
-#cd ..
-#rm -rf check/
-## shellcheck disable=SC2164
-#mkdir check && cd check
-#mkdir aluno && mkdir gabarito
-#cd ..
-#cp alunodetail/*_tab.txt check/aluno/
-#cp detail/*_tab.txt check/gabarito/
-#kdiff3 check/aluno/ check/gabarito
